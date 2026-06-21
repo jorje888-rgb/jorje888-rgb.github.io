10459ae1194b6d5f75662d027fcd4af3ef5fc910
jorje888-rgb <jirehkanon@gmail.com>
2026-06-21 12:39:17 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("nav a").forEach(a=>{
+a.addEventListener("click",e=>{
+e.preventDefault();
+document.querySelector(a.getAttribute("href"))
+.scrollIntoView({behavior:"smooth"});
+});
+});
